63d2a28407cef7948690ab39bf3731ab7f994226
Eduardo377 <eduardogomes377@gmail.com>
2023-03-27 22:19:41 -0300

    fix: exercicio do-while

--- loops-e-arrays/src/br/com/dio/exercicios/loops/Ex2_Nota.java
+++ loops-e-arrays/src/br/com/dio/exercicios/loops/Ex2_Nota.java
@@ -1,11 +1,25 @@
 package br.com.dio.exercicios.loops;
 
+import java.util.Scanner;
+
 public class Ex2_Nota {
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int nota;
+
+        int nota = 0;
 
         System.out.println("Nota: ");
-        nota = scan.NextInt();
+        nota = scan.nextInt();
+
+        while (nota < 0 || nota > 10) {
+            switch (nota) {
+                case null:System.out.println("Valor invalido!");
+                    break;
+                default:nota = scan.nextInt();
+                    break;
+            }
+            
+        }
     }
 }
